e9810e63ef61a50a4ca638d4582a3e73639811bf
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2021-12-09 11:34:52 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/user", (req, res) => {
+  let body = req.body;
+  res.send(body);
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
