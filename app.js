a1034ccb02aeb096b4dce75fb33f0a1f58794755
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2021-12-13 14:25:03 +0000

    m

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 const express = require("express");
 const app = express();
+
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 5000;
-
+const cors = require("cors");
 app.use(bodyParser.json());
+app.use(cors());
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+mongoose.connect(process.env.MONGODB_URI, () => {
+  console.log("connected to database");
 });
 
-app.post("/user", (req, res) => {
-  let body = req.body;
-  res.send(body);
+const todoSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  isCompleted: Boolean,
 });
 
-app.post("/user", (req, res) => {
-  let body = req.body;
-  res.send(body);
+const Todo = mongoose.model("Todo", todoSchema);
+
+app.get("/todo", async (req, res) => {
+  // res.send("hello world");
+  const todos = await Todo.find();
+  res.send(todos);
 });
 
-app.listen(port, () => {
-  console.log("Server is running on port 3000");
+app.post("/todo", async (req, res) => {
+  try {
+    const { title, body, isCompleted } = req.body;
+    const todo = new Todo();
+    todo.title = title;
+    todo.body = body;
+    todo.isCompleted = isCompleted;
+
+    const t = await todo.save();
+
+    res.send(t);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("running on port " + process.env.PORT);
 });
